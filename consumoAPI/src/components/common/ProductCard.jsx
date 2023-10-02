f8e43845c040e60a8fce77f3492db2dcc81ec1af
Estefania <estefaniaballesteros201@gmail.com>
2023-10-01 21:29:22 -0500

    adicion del detalle de producto y finalizacion del proyecto

--- consumoAPI/src/components/common/ProductCard.jsx
+++ consumoAPI/src/components/common/ProductCard.jsx
@@ -2,15 +2,16 @@
 import '../../styles/ProductCard.css'
 import { Link } from 'react-router-dom' //para reemplazar el href del la tag a para que no recargue la pag
 
-export const ProductCard = ({id, title,price,image}) => {
+export const ProductCard = ({id, title,price,image, description}) => {
   return (
-    <div className='container'>
+    <div className={description ? 'containerDescription' : 'container'}>
       <div>
         <Link to={`/products/${id}`}>
           {title}
         </Link>
       </div>  
         <img src={image} alt={title} />
+        {description && <p className='description'>{description}</p>}
         <p>$ {price} COP</p>
     </div>
   )
--- consumoAPI/src/components/common/ProductDetail.jsx
+++ consumoAPI/src/components/common/ProductDetail.jsx
@@ -1,11 +1,39 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import '../../styles/ProductDetail.css'
 import { useParams } from 'react-router-dom'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { ProductCard } from './ProductCard';
 
 export const ProductDetail = () => {
-  
+  const [product, setProduct] = useState({})
   let { id } = useParams();
+
+  const API= `https://fakestoreapi.com/products/${id}`
+
+  useEffect(() => {
+    axios.get(API).then(result => {
+      console.log(result.data);
+      return(
+        setProduct(result.data)
+      )
+  })
+  }, [])
+  
+
   
   return (
-    <div>Información del producto con identificador {id}</div>
+    <div>
+    <h2>Información del producto con identificador {id}</h2>
+    { product && 
+      <ProductCard 
+        id={product.id} 
+        title={product.title} 
+        image={product.image} 
+        price={product.price}
+        description={product.description}
+        />}
+    </div>
+    
   )
 }
